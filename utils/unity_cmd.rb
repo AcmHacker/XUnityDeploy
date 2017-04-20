69ebfe57a9a3d8e3019fbc90e3e298d06a5dd4de
tim <tim@acmhacker.com>
2017-04-20 15:28:10 +0800

    [*]修改unity cmd

--- utils/unity_cmd.rb
+++ utils/unity_cmd.rb
@@ -4,17 +4,17 @@
 module XUnityDeploy
     class UnityCmd
         def initialize method
-            @project_path = UnityProjectPath
             @method = method
         end
 
         def build
+            project_path = UnityProjectPath
             unity = "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
             if DeployOptions[:is_log] then
-                @log_path = File.join(LogPath, "deploy_#{DeployOptions[:platform]}.log")
-                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{@project_path} | tee #{@log_path}"
+                log_path = File.join(LogPath, "deploy_#{DeployOptions[:platform]}.log")
+                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{project_path} | tee #{log_path}"
             else
-                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{@project_path}"
+                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{project_path}"
             end
             cmd.sys_call_with_log
         end
