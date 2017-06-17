5baec9da90567946e01bf8f89a9a3774937c4a56
tim <tim@acmhacker.com>
2017-06-17 22:07:24 +0800

    [*]update xunitydeploy

--- unitys/compile_unity_renchang.rb
+++ unitys/compile_unity_renchang.rb
@@ -16,7 +16,7 @@ module XUnityDeploy
             compile_unity
 
             # support il8n
-            support_il8n
+            # support_il8n
 
             #config 
             config_capabilities            
