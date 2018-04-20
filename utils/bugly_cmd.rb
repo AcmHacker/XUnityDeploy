06357616884f84927735935cd2ecc527094fd670
tim <tim@acmhacker.com>
2018-04-20 14:37:30 +0800

    [*]修改bugly的version为main_version + build_version

--- utils/bugly_cmd.rb
+++ utils/bugly_cmd.rb
@@ -1,5 +1,6 @@
 #encoding = utf-8
 require 'json'
+require 'plist'
 
 # call unity command to build unity project
 module XUnityDeploy
@@ -15,13 +16,20 @@ module XUnityDeploy
             project_path = UnityProjectPath
             bugly = File.join(ToolPath, "bugly", "buglySymboliOS.jar")
             dysm_path = File.join(BuildPath, "ios.xcarchive", "dSYMs", "cannon.app.dSYM")
-            bugly_config = JSON.parse(File.read(File.join(ConfigPath, "bugly.json")))
-            deploy_config = JSON.parse(File.read(File.join(ConfigPath, "unity_deploy.json")))
-            main_version = deploy_config["ios"]["Version"]
-            build_version = File.read(File.join(UnityProjectPath, "Assets", "Resources", "info.txt"))
-            version = main_version + "." + build_version
+            info_plist =  Plist.parse_xml(File.join(BuildPath, "ios.xcarchive", "Info.plist"))
+            info_property = info_plist["ApplicationProperties"]
 
-            cmd = "java -jar #{bugly} -i #{dysm_path} -u -id #{bugly_config["appId"]} -key #{bugly_config["appKey"]} -package #{deploy_config["ios"]["BundleIdentifier"]} -version #{version}"
+            
+            # deploy_config = JSON.parse(File.read(File.join(ConfigPath, "unity_deploy.json")))
+            # main_version = deploy_config["ios"]["Version"]
+            # build_version = File.read(File.join(UnityProjectPath, "Assets", "Resources", "info.txt"))
+            # version = main_version + "." + build_version
+            version = info_property["CFBundleShortVersionString"] + "." + info_property["CFBundleVersion"]
+            package_name  = info_property["CFBundleIdentifier"]
+
+            bugly_config = JSON.parse(File.read(File.join(ConfigPath, "bugly.json")))
+
+            cmd = "java -jar #{bugly} -i #{dysm_path} -u -id #{bugly_config["appId"]} -key #{bugly_config["appKey"]} -package #{package_name} -version #{version}"
             # puts cmd
             cmd.sys_call_with_log
         end
