7eed044f050affd2ee9134a76c76620f2f158fb8
tim <tim@acmhacker.com>
2019-04-29 21:17:52 +0800

    [*]fix eny problem

--- utils/unity_cmd.rb
+++ utils/unity_cmd.rb
@@ -13,9 +13,8 @@ module XUnityDeploy
             # get unity command path by env yaml
             env = []
             env = YAML.load_file(EnvPath) if File.exist? EnvPath
-            @unity = env["unity3d"] ||= "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
-    
-            unity = "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
+            unity = env["unity3d"] ||= "/Applications/Unity/Unity.app/Contents/MacOS/Unity"    
+            # unity = "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
             if DeployOptions[:is_log] then
                 log_path = File.join(LogPath, "deploy_#{DeployOptions[:platform]}.log")
                 cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{project_path} | tee #{log_path}"
