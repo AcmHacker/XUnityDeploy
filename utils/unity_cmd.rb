1e7d850bba3e8a3c3d814b34b2a05d33f1254dee
NTRenChang <501joy@501joy-mac-mini.local>
2019-11-07 00:21:45 +0800

    [*]修改unity logFile to console

--- utils/unity_cmd.rb
+++ utils/unity_cmd.rb
@@ -17,9 +17,9 @@ module XUnityDeploy
             # unity = "/Applications/Unity/Unity.app/Contents/MacOS/Unity"
             if DeployOptions[:is_log] then
                 log_path = File.join(LogPath, "deploy_#{DeployOptions[:platform]}.log")
-                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{project_path} | tee #{log_path}"
+                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile - -projectPath #{project_path} | tee #{log_path}"
             else
-                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile -projectPath #{project_path}"
+                cmd = "#{unity} -batchmode -executeMethod #{@method} -quit -logFile - -projectPath #{project_path}"
             end
             cmd.sys_call_with_log
         end
